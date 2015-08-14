f4fa62ab2efdf7b06b8f1b4fefd88cbb5f39a37e
Adeola Ojo <ojoadeolagabriel@gmail.com>
2015-08-14 15:15:29 +0100

    sftp & xmlToJson components

---
+++ app.core/workflow/component/core/sftp/SftpEndpoint.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace app.core.workflow.component.core.sftp
+{
+    class SftpEndpoint
+    {
+    }
+}
