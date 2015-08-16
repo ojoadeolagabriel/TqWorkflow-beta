3d36f1d9e7ae9905e331db94abb75c23138cfebc
adeola <ojoadeolagabriel@gmail.com>
2015-08-16 08:39:45 +0100

    Seda queue...

---
+++ app.core/workflow/facade/Seda.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using app.core.workflow.dto;
+
+namespace app.core.workflow.facade
+{
+    public class Seda
+    {
+        public ConcurrentQueue<Exchange> SedaQueue = new ConcurrentQueue<Exchange>();
+    }
+}
