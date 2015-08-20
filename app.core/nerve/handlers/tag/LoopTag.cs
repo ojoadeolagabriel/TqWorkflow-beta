98392f48b8ae83d93e166028b558af6387a6d440
Adeola Ojo <ojoadeolagabriel@gmail.com>
2015-08-20 15:07:44 +0100

    loop tag..

--- app.core/nerve/handlers/tag/LoopTag.cs
+++ app.core/nerve/handlers/tag/LoopTag.cs
@@ -5,14 +5,39 @@ using System.Text;
 using System.Xml.Linq;
 using app.core.nerve.dto;
 using app.core.nerve.expression;
+using app.core.nerve.facade;
 
 namespace app.core.nerve.handlers.tag
 {
+    /// <summary>
+    /// Loop Tag Class.
+    /// </summary>
     public class LoopTag
     {
+        /// <summary>
+        /// Execute
+        /// </summary>
+        /// <param name="loopTag"></param>
+        /// <param name="exchange"></param>
+        /// <param name="route"></param>
         public static void Execute(XElement loopTag, Exchange exchange, Route route)
         {
-            
+            var expressionTag = loopTag.Elements().FirstOrDefault();
+            if (expressionTag == null)
+                return;
+
+            var xpression = expressionTag.Value;
+            var count = SimpleExpression.ObjectExpressionResolver(xpression);
+
+            var mCount = Convert.ToInt32(count);
+            for (var i = 0; i < mCount; i++)
+            {
+                var data = loopTag.Elements().Skip(1);
+                foreach (var dataItem in data)
+                {
+                    RouteStep.ProcessStep(dataItem, route, exchange);
+                }
+            }
         }
     }
 }
