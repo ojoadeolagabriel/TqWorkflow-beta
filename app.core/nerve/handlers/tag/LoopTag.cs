5c28815a8d70f8237ca6ef18eccd410c29c7b9bf
Adeola Ojo <ojoadeolagabriel@gmail.com>
2015-08-20 14:48:23 +0100

    loop tag

---
+++ app.core/nerve/handlers/tag/LoopTag.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using app.core.nerve.dto;
+using app.core.nerve.expression;
+
+namespace app.core.nerve.handlers.tag
+{
+    public class LoopTag
+    {
+        public static void Execute(XElement loopTag, Exchange exchange, Route route)
+        {
+            
+        }
+    }
+}
