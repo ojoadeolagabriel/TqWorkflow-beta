c9cbcd2b9988297b722c45dbe6557f0f0921bb44
adeola <ojoadeolagabriel@gmail.com>
2015-08-26 22:00:03 +0100

    CacheEndpoint support

---
+++ app.core/nerve/component/core/cache/CacheEndpoint.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using app.core.nerve.dto;
+using app.core.nerve.facade;
+
+namespace app.core.nerve.component.core.cache
+{
+    public class CacheEndpoint : DefaultEndpoint
+    {
+        public CacheEndpoint(string uri, Route route)
+            : base(uri, route)
+        {
+        }
+
+        public override void Send(Exchange exchange, UriDescriptor endPointDescriptor)
+        {
+            var producer = CreateProducer();
+            producer.Process(exchange, endPointDescriptor);
+        }
+
+        public DefaultProducer CreateProducer()
+        {
+            return new CacheProducer(UriInformation, Route);
+        }
+    }
+}
---
+++ app.core/nerve/component/core/cache/CacheProducer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using app.core.nerve.dto;
+using app.core.nerve.facade;
+
+namespace app.core.nerve.component.core.cache
+{
+    public class CacheProducer : DefaultProducer
+    {
+        public CacheProducer(UriDescriptor uriInformation, Route route) : base(uriInformation, route)
+        {
+        }
+
+        public override Exchange Process(Exchange exchange, UriDescriptor descriptor)
+        {
+            Camel.TryLog(exchange, "producer", descriptor.ComponentName);
+            var action = descriptor.ComponentPath ?? "add";
+
+            try
+            {
+                switch (action)
+                {
+                    case "add":
+                        var coll = UriDescriptor.BuildKeyValueListWithEquality(descriptor.ComponentQueryPath);
+                        break;
+                    default:
+                        break;
+                }
+            }
+            catch
+            {
+                
+            }
+            return exchange;
+        }
+    }
+}
