f660602994cbbf88c57dbf3f49d7ca5c3850dcb8
adeola <ojoadeolagabriel@gmail.com>
2015-09-05 15:53:38 +0100

    tcp processor

---
+++ camelcontext.server/facade/network/BasicTcpProcessor.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Xml.Linq;
+using app.core.nerve.dto;
+using app.core.nerve.facade;
+
+namespace camelcontext.server.facade.network
+{
+    public class BasicTcpProcessor : ProcessorBase
+    {
+        public override void Process(Exchange exchange)
+        {
+            var body = exchange.InMessage.Body.ToString();
+
+            if (body.EndsWith("\\r\\n"))
+                body = body.Replace("\\r\\n", "");
+
+            
+            var xml = XElement.Parse(body);
+            xml.Add(new XElement("response", "00"));
+            exchange.InMessage.Body = xml.ToString();
+        }
+    }
+}
