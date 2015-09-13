d3aa5773a05a3419da561eefb8596f18cff53553
adeola <ojoadeolagabriel@gmail.com>
2015-09-13 22:54:40 +0100

    mongo

--- app.core/nerve/component/core/mongodb/MongoDbProducer.cs
+++ app.core/nerve/component/core/mongodb/MongoDbProducer.cs
@@ -4,26 +4,55 @@ using System.Linq;
 using System.Text;
 using app.core.nerve.dto;
 using app.core.nerve.facade;
+using MongoDB.Bson;
 using MongoDB.Driver;
 
 namespace app.core.nerve.component.core.mongodb
 {
     public class MongoDbProducer : DefaultProducer
     {
-        public MongoDbProducer(UriDescriptor uriInformation, Route route) : base(uriInformation, route)
+        public enum OperationType
+        {
+            FetchById, Insert
+        }
+
+        public MongoDbProducer(UriDescriptor uriInformation, Route route)
+            : base(uriInformation, route)
         {
         }
 
         public override Exchange Process(Exchange exchange, UriDescriptor endPointDescriptor)
         {
             var host = endPointDescriptor.GetUriProperty("host");
+            var database = endPointDescriptor.GetUriProperty("database");
+            var collection = endPointDescriptor.GetUriProperty("collection");
+            var createCollection = endPointDescriptor.GetUriProperty<bool>("createCollection");
+            var operation = endPointDescriptor.GetUriProperty<OperationType>("operation");
+
             var client = new MongoClient("mongodb://" + host);
-            var db = client.GetServer().GetDatabase("local");
-            var coll = db.CreateCollection("user_info");
-            
-            //var data = coll.FindAll();
+            var db = client.GetServer().GetDatabase(database);
+
+            if (!db.CollectionExists(collection) && createCollection)
+                db.CreateCollection(collection);
+
+            var doc = BsonDocument.Parse(exchange.InMessage.Body.ToString());
+
+            ServeOperation(db, exchange, endPointDescriptor, doc, operation, collection);
 
             return exchange;
+        }
+
+        private void ServeOperation(MongoDatabase db, Exchange exchange, UriDescriptor endPointDescriptor, 
+            BsonDocument doc, OperationType operation, string collection)
+        {
+            switch (operation)
+            {
+                case OperationType.Insert:
+                    db.GetCollection(collection).Insert(doc);
+                    break;
+                case OperationType.FetchById:
+                    break;
+            }
         }
     }
 }
---
+++ app.core/nerve/sys/MongoLogger.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using app.core.nerve.dto;
+
+namespace app.core.nerve.sys
+{
+    public class MongoLogger : ISystemLogProvider
+    {
+        public void Log(Exchange exchange, string processorType, string componentName, Exception exception = null)
+        {
+            
+        }
+    }
+}
