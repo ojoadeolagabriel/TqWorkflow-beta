eda23821680b9031e59a80d7d2c967a3aca4514f
adeola <ojoadeolagabriel@gmail.com>
2015-09-13 17:14:49 +0100

    mongo db online..

--- app.core/nerve/component/core/mongodb/MongoDbProducer.cs
+++ app.core/nerve/component/core/mongodb/MongoDbProducer.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using app.core.nerve.dto;
 using app.core.nerve.facade;
+using MongoDB.Driver;
 
 namespace app.core.nerve.component.core.mongodb
 {
@@ -15,6 +16,13 @@ namespace app.core.nerve.component.core.mongodb
 
         public override Exchange Process(Exchange exchange, UriDescriptor endPointDescriptor)
         {
+            var host = endPointDescriptor.GetUriProperty("host");
+            var client = new MongoClient("mongodb://" + host);
+            var db = client.GetServer().GetDatabase("local");
+            var coll = db.CreateCollection("user_info");
+            
+            //var data = coll.FindAll();
+
             return exchange;
         }
     }
