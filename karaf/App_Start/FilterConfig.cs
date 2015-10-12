43482db25fc3eff07cee527af72715762cb27a9c
adeola <ojoadeolagabriel@gmail.com>
2015-10-12 04:07:38 +0100

    web app

---
+++ karaf/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace karaf
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
