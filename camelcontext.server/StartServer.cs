f12519f087204e705753a33f7fad309a163d0de2
Adeola Ojo <ojoadeolagabriel@gmail.com>
2016-05-15 20:54:38 +0100

    no message

--- camelcontext.server/StartServer.cs
+++ camelcontext.server/StartServer.cs
@@ -18,14 +18,14 @@ namespace camelcontext.server
     {
         static void Main(string[] args)
         {
-            const string rootPath = @"C:\Users\AdeolaOjo\Documents\Visual Studio 2013\Projects";
-            Camel.LoadBundle(new List<string>{ String.Format(@"{0}\TqWorkflow-beta\tcp.consumer.bundle\bin\Debug\tcp.consumer.bundle.dll", rootPath) });
-            Camel.LoadBundle(new List<string> { string.Format(@"{0}\TqWorkflow-beta\mina.nibss.consumer.bunde\bin\Debug\mina.nibss.consumer.bundle.dll", rootPath) });
-            Camel.LoadBundle(new List<string> { string.Format(@"{0}\TqWorkflow-beta\ClassLibrary1\bin\Debug\paymentnotification.generic.bundle.dll", rootPath) });
-            Camel.LoadBundle(new List<string> { String.Format(@"{0}\TqWorkflow-beta\autopay.ftp.consumer.bundle\bin\Debug\autopay.ftp.consumer.bundle.dll", rootPath) });
-            Camel.LoadBundle(new List<string> { String.Format(@"{0}\TqWorkflow-beta\softalliance.processor.bundle\bin\Debug\softalliance.processor.bundle.dll", rootPath) });
-            Camel.LoadBundle(new List<string> { String.Format(@"{0}\TqWorkflow-beta\isofield.transformer.bundle\bin\Debug\isofield.transformer.bundle.dll", rootPath) });
-            Camel.LoadBundle(new List<string> { String.Format(@"{0}\TqWorkflow-beta\autopay.transactor.bundle\bin\Debug\autopay.transactor.bundle.dll", rootPath) });
+            const string rootPath = @"C:\Users\AdeolaOjo\Documents\workflow";
+            Camel.LoadBundle(new List<string>{ String.Format(@"{0}\tcp.consumer.bundle\bin\Debug\tcp.consumer.bundle.dll", rootPath) });
+            Camel.LoadBundle(new List<string> { string.Format(@"{0}\mina.nibss.consumer.bunde\bin\Debug\mina.nibss.consumer.bundle.dll", rootPath) });
+            Camel.LoadBundle(new List<string> { string.Format(@"{0}\ClassLibrary1\bin\Debug\paymentnotification.generic.bundle.dll", rootPath) });
+            Camel.LoadBundle(new List<string> { String.Format(@"{0}\autopay.ftp.consumer.bundle\bin\Debug\autopay.ftp.consumer.bundle.dll", rootPath) });
+            Camel.LoadBundle(new List<string> { String.Format(@"{0}\softalliance.processor.bundle\bin\Debug\softalliance.processor.bundle.dll", rootPath) });
+            Camel.LoadBundle(new List<string> { String.Format(@"{0}\isofield.transformer.bundle\bin\Debug\isofield.transformer.bundle.dll", rootPath) });
+            Camel.LoadBundle(new List<string> { String.Format(@"{0}\autopay.transactor.bundle\bin\Debug\autopay.transactor.bundle.dll", rootPath) });
         
             Camel.StartEngine();
             SystemBundleManagement.StartService("http://127.0.0.1:7098/api/v2/app.management.api/");
